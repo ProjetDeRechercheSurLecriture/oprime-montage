f81015ea766caab558ebccaedb949a8f47fa2f77
cesine <cesine@yahoo.com>
2014-08-22 08:27:33 -0400

    adding participants list for https://github.com/ProjetDeRechercheSurLecriture/DyslexDisorthGame/issues/174

---
+++ ui/participants-select.reel/participants-select.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/participants-select.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class ParticipantsSelect
+ * @extends Component
+ */
+exports.ParticipantsSelect = Component.specialize(/** @lends ParticipantsSelect# */ {
+    constructor: {
+        value: function ParticipantsSelect() {
+            this.super();
+        }
+    }
+});
