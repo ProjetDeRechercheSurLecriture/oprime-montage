7923b36794cc5aca65cf073e9d009732f21cfaf1
cesine <cesine@yahoo.com>
2014-08-26 09:01:47 -0400

    sound check extends confirm dialog for
    https://github.com/ProjetDeRechercheSurLecriture/DyslexDisorthGame/issues/176

--- ui/sound-check.reel/sound-check.js
+++ ui/sound-check.reel/sound-check.js
@@ -1,14 +1,14 @@
 /**
  * @module ui/sound-check.reel
- * @requires montage/ui/component
+ * @requires ui/Confirm
  */
-var Component = require("montage/ui/component").Component;
+var Confirm = require("ui/confirm.reel").Confirm;
 
 /**
  * @class SoundCheck
- * @extends Component
+ * @extends Confirm
  */
-exports.SoundCheck = Component.specialize(/** @lends SoundCheck# */ {
+exports.SoundCheck = Confirm.specialize(/** @lends SoundCheck# */ {
     constructor: {
         value: function SoundCheck() {
             this.super();
