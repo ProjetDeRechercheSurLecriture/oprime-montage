3cc1b87033d008698b08c59fba42b0714a16df65
cesine <cesine@yahoo.com>
2014-08-25 20:11:04 -0400

    creating sound check component for https://github.com/ProjetDeRechercheSurLecriture/DyslexDisorthGame/issues/176

---
+++ ui/sound-check.reel/sound-check.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/sound-check.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class SoundCheck
+ * @extends Component
+ */
+exports.SoundCheck = Component.specialize(/** @lends SoundCheck# */ {
+    constructor: {
+        value: function SoundCheck() {
+            this.super();
+        }
+    }
+});
