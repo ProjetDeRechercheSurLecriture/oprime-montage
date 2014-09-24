ddb84bbf87a30676f4e9baeb9fdafbea7ae9041c
cesine <cesine@yahoo.com>
2014-09-24 15:39:02 -0400

    turning off warnings in all fielddb objects

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -3,7 +3,8 @@
  * @requires core/contextualizable-component
  */
 var ContextualizableComponent = require("core/contextualizable-component").ContextualizableComponent,
-	Contextualizer = require("core/experiment-contextualizer").Contextualizer;
+	Contextualizer = require("core/experiment-contextualizer").Contextualizer,
+	FieldDBObject = require("fielddb/api/FieldDBObject").FieldDBObject;
 
 var enLocales = require("locale/en/messages.json");
 var frLocales = require("locale/fr/messages.json");
@@ -17,6 +18,9 @@ exports.Main = ContextualizableComponent.specialize( /** @lends Main# */ {
 			// localStorage.setItem("montage_locale", "fr");
 			this.super();
 
+			FieldDBObject.warn = function(){
+				//dont warn
+			};
 			if (!this.application.contextualizer) {
 				this.application.contextualizer = this.application.contextualizer || new Contextualizer();
 				this.application.contextualizer.addMessagesToContextualizedStrings("en", enLocales);
