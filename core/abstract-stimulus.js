1ecbca35e88c166b48bdebea22a72bf7ab97ff31
cesine <cesine@yahoo.com>
2014-09-26 11:02:14 -0400

    reverting 4b219f03c2e27f0bebebb8fe87194b550e97123a to return to abstract stimulus as a montage component

--- core/abstract-stimulus.js
+++ core/abstract-stimulus.js
@@ -2,41 +2,23 @@
  * @module ui/stimulus.reel
  * @requires montage/ui/component
  */
-var Component = require("montage/ui/component").Component,
-	Target = require("montage/core/target").Target,
-	Montage = require("montage/montage").Montage,
-	Stimulus = require("fielddb/api/datum/Stimulus").Stimulus,
-	FieldDBResponse = require("fielddb/api/datum/Response").Response,
-	// Confirm = require("ui/confirm.reel").Confirm,
+var ContextualizableComponent = require("core/contextualizable-component").ContextualizableComponent,
+	Confirm = require("ui/confirm.reel").Confirm,
 	Response = require("ui/response.reel").Response,
 	PressComposer = require("montage/composer/press-composer").PressComposer,
 	RangeController = require("montage/core/range-controller").RangeController,
 	Promise = require("montage/core/promise").Promise;
 
 /**
- * @class AbstractStimulus
- * @extends FieldDBResponse
+ * @class Stimulus
+ * @extends ContextualizableComponent
  */
-
-var AbstractStimulusPrototype = {
+exports.AbstractStimulus = ContextualizableComponent.specialize( /** @lends Stimulus# */ {
 	constructor: {
-		value: function() {
-			// this.debug = function(message) {
-			// 	console.warn("OVERRIDEN Debug " + message);
-			// };
-			// this.verbose = function(message) {
-			// 	console.warn("OVERRIDEN verbose " + message);
-			// };
-			var options =  new FieldDBResponse();
-			console.log("constructing with ",options);
-			FieldDBResponse.apply(this, options);
+		value: function Stimulus() {
 			this.super();
-		}
-	},
+			this.responses = [];
 
-	contextualizer: {
-		get: function() {
-			return this.application.contextualizer;
 		}
 	},
 
@@ -55,8 +37,185 @@ var AbstractStimulusPrototype = {
 	//    },
 
 	responsesController: {
-		value: null,
-		configurable: true
+		value: null
+	},
+
+	responses: {
+		value: null
+	},
+	pauseAudioWhenConfirmingResponse: {
+		value: null
+	},
+	addResponse: {
+		value: function(responseEvent, stimulusId) {
+			if (!responseEvent) {
+				throw "Cannot add response without the x y information found in the touch/click responseEvent";
+			}
+
+			var reactionTimeEnd = Date.now();
+			var audioDuration = this.application.audioPlayer.getDuration(this.audioFile) || 0;
+			if (audioDuration) {
+				audioDuration = audioDuration * 1000;
+			} else {
+				console.log("The audio has no duration.. This is strange.");
+			}
+			if (this.pauseAudioWhenConfirmingResponse) {
+				this.pauseAudio();
+			}
+
+			var self = this;
+			var continueToNextStimulus = Promise.defer();
+			if (this.confirmResponseChoiceMessage) {
+				this.contextualizer.currentLocale = this.application.interfaceLocale;
+				var confirmChoicePrompt = this.contextualizer.localize(this.confirmResponseChoiceMessage);
+				var options = {
+					iconSrc: self.ownerComponent.iconSrc,
+					message: confirmChoicePrompt
+				};
+				Confirm.show(options, function() {
+					continueToNextStimulus.resolve();
+				}, function() {
+					continueToNextStimulus.reject(new Error("The x prevented the cancel?"));
+				});
+			} else {
+				continueToNextStimulus.resolve();
+			}
+			continueToNextStimulus.promise.then(function() {
+				// self.ownerComponent.templateObjects.reinforcement.next();
+				self.stopAudio();
+				self.ownerComponent.nextStimulus();
+			}, function(reason) {
+				console.log("Not continuing to next stimulus");
+				if (this.pauseAudioWhenConfirmingResponse) {
+					self.playAudio();
+				}
+			});
+			var choice = "";
+			if (stimulusId) {
+				choice = this[stimulusId].substring(this[stimulusId].lastIndexOf("/") + 1).replace(/\..+$/, "").replace(/\d+_/, "");
+				if (choice === this.target.orthographic) {
+					choice = this.target;
+				} else {
+					this.distractors.map(function(distractor) {
+						if (choice === distractor.orthographic) {
+							choice = distractor;
+						}
+					});
+				}
+			}
+			var response = {
+				"reactionTimeAudioOffset": reactionTimeEnd - this.reactionTimeStart - audioDuration,
+				"reactionTimeAudioOnset": reactionTimeEnd - this.reactionTimeStart,
+				"x": responseEvent.x,
+				"y": responseEvent.y,
+				"pageX": responseEvent.pageX,
+				"pageY": responseEvent.pageY,
+				// "prime": {
+				// 	phonemic: this.prime.phonemic,
+				// 	orthographic: this.prime.orthographic,
+				// 	imageFile: this.prime.imageFile
+				// },
+				"choice": choice,
+				// "target": this.target,
+				"score": this.scoreResponse(this.target, choice)
+			};
+			this.responses.push(response);
+			console.log("Recorded response", JSON.stringify(response));
+		}
+	},
+
+	addOralResponse: {
+		value: function(choice, dontAutoAdvance) {
+			var reactionTimeEnd = Date.now();
+			var audioDuration = this.application.audioPlayer.getDuration(this.audioFile) || 0;
+			if (audioDuration) {
+				audioDuration = audioDuration * 1000;
+			} else {
+				console.log("The audio has no duration.. This is strange.");
+			}
+			if (this.pauseAudioWhenConfirmingResponse) {
+				this.pauseAudio();
+			}
+
+			var self = this;
+			var continueToNextStimulus = Promise.defer();
+			if (this.confirmResponseChoiceMessage) {
+				this.contextualizer.currentLocale = this.application.interfaceLocale;
+				var confirmChoicePrompt = this.contextualizer.localize(this.confirmResponseChoiceMessage);
+				var options = {
+					iconSrc: self.ownerComponent.iconSrc,
+					message: confirmChoicePrompt
+				};
+				Confirm.show(options, function() {
+					continueToNextStimulus.resolve();
+				}, function() {
+					continueToNextStimulus.reject(new Error("The x prevented the cancel?"));
+				});
+			} else {
+				if (!dontAutoAdvance) {
+					continueToNextStimulus.resolve();
+				}
+			}
+			continueToNextStimulus.promise.then(function() {
+				// self.ownerComponent.templateObjects.reinforcement.next();
+				self.stopAudio();
+				self.ownerComponent.nextStimulus();
+			}, function(reason) {
+				console.log("Not continuing to next stimulus");
+				if (this.pauseAudioWhenConfirmingResponse) {
+					self.playAudio();
+				}
+			});
+
+			var response = {
+				"reactionTimeAudioOffset": reactionTimeEnd - this.reactionTimeStart - audioDuration,
+				"reactionTimeAudioOnset": reactionTimeEnd - this.reactionTimeStart,
+				"x": 0,
+				"y": 0,
+				"pageX": 0,
+				"pageY": 0,
+				"choice": choice,
+				"score": choice.score
+			};
+			this.responses = this.responses || [];
+			this.responses.push(response);
+			console.log("Recorded response", JSON.stringify(response));
+		}
+	},
+
+	scoreResponse: {
+		value: function(expectedResponse, actualResponse) {
+			if (!actualResponse.orthographic) {
+				return "error";
+			}
+			if (actualResponse.orthographic === expectedResponse.orthographic) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+	},
+
+	addNonResponse: {
+		value: function(responseEvent) {
+			if (!responseEvent) {
+				throw "Cannot add response without the x y information found in the touch/click responseEvent";
+			}
+			var reactionTimeEnd = Date.now();
+			var response = {
+				"reactionTimeAudioOffset": reactionTimeEnd - this.reactionTimeStart,
+				"reactionTimeAudioOnset": reactionTimeEnd - this.reactionTimeStart,
+				"x": responseEvent.x,
+				"y": responseEvent.y,
+				"pageX": responseEvent.pageX,
+				"pageY": responseEvent.pageY,
+				"chosenVisualStimulus": "none",
+				"responseScore": -1
+			};
+			this.responses = this.responses || [];
+			this.nonResponses.push(response);
+			console.log("Recorded non-response, the user is confused or not playing the game.", JSON.stringify(response));
+		}
 	},
 
 	/*
@@ -67,8 +226,7 @@ var AbstractStimulusPrototype = {
 	 */
 	enterDocument: {
 		value: function(firstTime) {
-			ContextualizableComponent.prototype.enterDocument.apply(this, arguments);
-			// this.super();
+			this.super();
 
 			if (firstTime) {
 				this.setupFirstPlay();
@@ -147,25 +305,47 @@ var AbstractStimulusPrototype = {
 		}
 	},
 
-	load: {
+	/**
+	 *  TODO try using a media controller later montage/ui/controller/media-controller
+	 * @type {Object}
+	 */
+	playAudio: {
+		value: function(delay) {
+			this.application.audioPlayer.play(this.audioFile, delay);
+		}
+	},
+
+	pauseAudio: {
 		value: function() {
-			FieldDBResponse.prototype.load.apply(this, []);
+			this.application.audioPlayer.pause(this.audioFile);
+		}
+	},
+
+	stopAudio: {
+		value: function() {
+			this.application.audioPlayer.stop(this.audioFile);
+		}
+	},
+
+	load: {
+		value: function(details) {
+			for (var d in details) {
+				if (details.hasOwnProperty(d)) {
+					this[d] = details[d];
+				}
+			}
+			if (this.responses === null) {
+				this.responses = [];
+			}
+			if (this.nonResponses === null) {
+				this.nonResponses = [];
+			}
+			this.nonResponses = [];
 			this.responsesController = new RangeController().initWithContent(this.responses);
+			this.experimenterId = this.application.experiment.experimenter.id;
+			this.participantId = this.application.experiment.participant.id;
+			// Not playing audio by default, child must call it.
+			// this.playAudio(2000);
 		}
 	}
-
-};
-
-MontagifiedFieldDBResponse = Montage.specialize(AbstractStimulusPrototype, FieldDBResponse.prototype)
-// var AbstractStimulus = function AbstractStimulus(options) {
-// 	this.debug("Constructing AbstractStimulus ", options);
-// 	// Target.prototype.constructor.apply(this, arguments);
-// 	// Component.prototype.constructor.apply(this, arguments);
-
-// 	FieldDBResponse.apply(this, arguments);
-
-// };
-
-// AbstractStimulus.prototype = Object.create(FieldDBResponse.prototype, /** @lends AbstractStimulus.prototype */ );
-
-exports.AbstractStimulus = MontagifiedFieldDBResponse;
+});
