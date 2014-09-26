0140c7f8ba1bf1aac3e480e6743cc902a7b1224a
cesine <cesine@yahoo.com>
2014-09-26 10:59:28 -0400

    updating result calculation to use the fielddb experimental design

--- core/data/result_only.js
+++ core/data/result_only.js
@@ -15,19 +15,19 @@ exports.map = function(doc) {
             var totalStimuli = 0;
             var totalAnswered = 0;
             var results = [];
-            for (var subexperimentIndex = 0; subexperimentIndex < doc.subexperiments.length; subexperimentIndex++) {
-                var subexperiment = doc.subexperiments[subexperimentIndex];
+            for (var subexperimentIndex = 0; subexperimentIndex < doc.subexperiments._collection.length; subexperimentIndex++) {
+                var subexperiment = doc.subexperiments._collection[subexperimentIndex];
                 subexperiment.scoreSubTotal = 0;
-                for (var stimulusIndex = 0; stimulusIndex < subexperiment.trials.length; stimulusIndex++) {
-                    var stimulusToScore = subexperiment.trials[stimulusIndex];
+                for (var stimulusIndex = 0; stimulusIndex < subexperiment.trials._collection.length; stimulusIndex++) {
+                    var stimulusToScore = subexperiment.trials._collection[stimulusIndex];
                     if (stimulusToScore.responses && stimulusToScore.responses[stimulusToScore.responses.length - 1] && stimulusToScore.responses[stimulusToScore.responses.length - 1].score !== undefined) {
                         stimulusToScore.response = stimulusToScore.responses[stimulusToScore.responses.length - 1];
                         stimulusToScore.score = stimulusToScore.responses[stimulusToScore.responses.length - 1].score;
                         results.push({
-                            prime: stimulusToScore.prime ? stimulusToScore.prime.phonemic : null,
-                            stimulus: stimulusToScore.stimulus ? stimulusToScore.stimulus.phonemic : null,
-                            target: stimulusToScore.target ? stimulusToScore.target.phonemic : null,
-                            response: stimulusToScore.response.choice ? stimulusToScore.response.choice.phonemic : null,
+                            prime: stimulusToScore.prime ? stimulusToScore.prime.utterance : null,
+                            stimulus: stimulusToScore.stimulus ? stimulusToScore.stimulus.utterance : null,
+                            target: stimulusToScore.target ? stimulusToScore.target.utterance : null,
+                            response: stimulusToScore.response.choice ? stimulusToScore.response.choice.utterance : null,
                             score: stimulusToScore.score
                         });
                         subexperiment.scoreSubTotal += stimulusToScore.score;
@@ -35,7 +35,7 @@ exports.map = function(doc) {
                     } else {
                         // stimulusToScore.response = {
                         //  response: {
-                        //      orthographic: "NA"
+                        //      orthography: "NA"
                         //  }
                         // };
                         // stimulusToScore.score = null;
@@ -44,7 +44,7 @@ exports.map = function(doc) {
                 }
                 if (true || subexperiment.label.indexOf("practice") === -1) {
                     totalScore += subexperiment.scoreSubTotal;
-                    totalStimuli += subexperiment.trials.length;
+                    totalStimuli += subexperiment.trials._collection.length;
                 }
             }
             emit(totalScore / totalAnswered, results);
