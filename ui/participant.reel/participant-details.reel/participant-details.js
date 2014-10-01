4253941dc17eeee1efd6d3852e0ebffe95184ba4
cesine <cesine@yahoo.com>
2014-10-01 15:39:29 -0400

    creating participant details component for https://github.com/ProjetDeRechercheSurLecriture/DyslexDisorthGame/issues/186

---
+++ ui/participant.reel/participant-details.reel/participant-details.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/participant-details.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class ParticipantDetails
+ * @extends Component
+ */
+exports.ParticipantDetails = Component.specialize(/** @lends ParticipantDetails# */ {
+    constructor: {
+        value: function ParticipantDetails() {
+            this.super();
+        }
+    }
+});
